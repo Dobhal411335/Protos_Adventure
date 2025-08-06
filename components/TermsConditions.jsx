0e0e13c93827bd51085153d49e6fda340d7610f0
Dobhal411335 <dobhalservices@gmail.com>
2025-08-06 22:22:02 +0530

    Update TermsConditions.jsx

--- components/TermsConditions.jsx
+++ components/TermsConditions.jsx
@@ -127,29 +127,23 @@ const TermsConditions = () => {
                 of payments offered. The purchase of products shall be additionally governed by specific policies of sale,
                 like cancellation policy, return policy, etc. and all of which are incorporated here by reference. In addition,
                 these terms and policies may be further supplemented by www.protosadventures.com (Website) product
-                specific conditions, which may be displayed on the webpage of that product.  <br /> <br />
-                “www.protosadventures.com (Website) ” is a registered trade mark.
-                <br />
-                PAN Number :- AADCP9394E1
-                <br />
-                GST Registration Number :- GSTIN: 05AADCP9394E1Z7
-                <br />
-                Working Hours : 9:30 - 18:30 pm
-                <br />
-                PROTOS ADVENTURE SPORTS & SECURITY SUPPLIES PVT LTD
-                <br />
-                Corporate Address: 05 ,Lajpat Rai Marg, Rishikesh-249201 ( Uttarakhand)
-                <br />
-                Store Address: 162, Badrinath Road (Opp. Divine Lakshmi Ganga Hotel)
-                <br />
-                Tapovan Luxman Jhoola, RISHIKESH-249 192 (UK)
-                <br />
-                Ph.: +91-135-2442822 | Mo.: 7669280002, 9897468886
-                <br />
-                E-mail : nitin.rksh@gmail.com / info@protosadventures.com
-                <br />
-                Visit us at : www.protosadventures.com
+                specific conditions, which may be displayed on the webpage of that product.  <br /> <br />   
               </p>
+
+                        <p className="font-semibold">"www.protosadventures.com" is a registered trade mark.</p>
+            <p>PAN Number: AADCP9394E1</p>
+            <p>GST Registration Number: 05AADCP9394E1Z7</p>
+            <p>Working Hours: 9:30 - 18:30 pm</p>
+            <p className="font-semibold mt-2">PROTOS ADVENTURE SPORTS & SECURITY SUPPLIES PVT LTD </br>
+              Corporate Address: 05 ,Lajpat Rai Marg, Rishikesh-249201 ( Uttarakhand)
+              <br/>
+              
+              Tapovan Luxman Jhoola, RISHIKESH-249 192 (UK)  </p>
+            <p>Ph.: 0135-2439354 | Mo.: 9897289601</p>
+            <p>E-mail: <a href="mailto:umeshgoel4@hotmail.com" className="text-blue-600 hover:underline">umeshgoel4@hotmail.com</a> / E-mail: <a href="mailto:info@protosadventures.com" className="text-blue-600 hover:underline">info@protosadventures.com</a> / <a href="mailto:nitin.rksh@gmail.com" className="text-blue-600 hover:underline">nitin.rksh@gmail.com</a></p>
+            <p>Visit us at: <a href="https://www.protosadventures.com" className="text-blue-600 hover:underline">www.protosadventures.com</a></p>
+
+                
             </div>
           </div>
         </div>

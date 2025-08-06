3e74904d083a5c8404585cbb09facf59ca76be7d
Dobhal411335 <dobhalservices@gmail.com>
2025-08-06 22:31:52 +0530

    Update ReturnPolicy.jsx

--- components/ReturnPolicy.jsx
+++ components/ReturnPolicy.jsx
@@ -125,7 +125,7 @@ const ReturnPolicy = () => {
           </div>
 
           <div className="border-t border-gray-300 pt-6 text-sm text-gray-600">
-                         <p className="font-semibold">"www.protosadventures.com" is a registered trade mark.</p>
+                                 <p className="font-semibold">"www.protosadventures.com" is a registered trade mark.</p>
             <p>PAN Number: AADCP9394E1</p>
             <p>GST Registration Number: 05AADCP9394E1Z7</p>
             <p>Working Hours: 9:30 - 18:30 pm</p>
@@ -135,7 +135,7 @@ const ReturnPolicy = () => {
               
               Tapovan Luxman Jhoola, RISHIKESH-249 192 (UK)  </p>
             <p>Ph.: 0135-2439354 | Mo.: 9897289601</p>
-            <p>E-mail: <a href="mailto:umeshgoel4@hotmail.com" className="text-blue-600 hover:underline">umeshgoel4@hotmail.com</a> / E-mail: <a href="mailto:info@protosadventures.com" className="text-blue-600 hover:underline">info@protosadventures.com</a> / <a href="mailto:nitin.rksh@gmail.com" className="text-blue-600 hover:underline">nitin.rksh@gmail.com</a></p>
+            <p>E-mail: <a href="mailto:umeshgoel4@hotmail.com" className="text-blue-600 hover:underline">umeshgoel4@hotmail.com</a> / E-mail: <a href="mailto:info@protosadventures.com" className="text-blue-600 hover:underline">info@protosadventures.com</a> </p>
             <p>Visit us at: <a href="https://www.protosadventures.com" className="text-blue-600 hover:underline">www.protosadventures.com</a></p>
           </div>
         </div>
